1b76ed3e62b36aba8253d6e34b3ed16a174cbf7f
MarufMehar <marufkhan1001gg@gmail.com>
2026-04-19 16:05:14 +0530

    socket connect by irfan

--- controllers/modules/common.controller.js
+++ controllers/modules/common.controller.js
@@ -1,5 +1,4 @@
 const { randomid } = require('../../Assets/javascript/function')
 const user = require('../../modules/database.connection');
 
-module.exports = {
-}
+module.exports = { }
--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// const express = require('express');
+// const app = express();
+// const helmet = require('helmet');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const rateLimit = require('express-rate-limit');
+// const compression = require('compression');
+// const cookieParser = require('cookie-parser');
+// require('./modules/database.connection');
+// const authRouter = require('./routes/auth/auth.route');
+// const moduleRouter = require('./routes/modules/module.route');
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(helmet());
+// app.use(cookieParser());
+// app.use(cors());
+// app.use(morgan('dev'));
+// app.use(compression());
+
+// app.use(rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 100
+// }));
+// app.use('/', authRouter);
+// app.use('/', moduleRouter);
+// app.use((req, res) => {
+//     res.status(404).json({
+//         msg: 'Route Not Found',
+//         status: 1
+//     });
+// });
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({
+//         msg: 'Something Went Wrong',
+//         status: 1
+//     });
+// });
+// const port = process.env.SERVER_PORT || 5001;
+// app.listen(port, () => console.log(`Server is running on ${port}`));
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
@@ -6,9 +46,13 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
+const http = require("http"); // ✅ ADD THIS
+
 require('./modules/database.connection');
+
 const authRouter = require('./routes/auth/auth.route');
 const moduleRouter = require('./routes/modules/module.route');
+const { initSocket } = require('./utils/socket');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
@@ -21,14 +65,17 @@ app.use(rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100
 }));
+
 app.use('/', authRouter);
 app.use('/', moduleRouter);
+
 app.use((req, res) => {
     res.status(404).json({
         msg: 'Route Not Found',
         status: 1
     });
 });
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({
@@ -36,5 +83,14 @@ app.use((err, req, res, next) => {
         status: 1
     });
 });
+
+
+// ✅ CREATE SERVER
+const server = http.createServer(app);
+
+initSocket(server);
+
+
+
 const port = process.env.SERVER_PORT || 5001;
-app.listen(port, () => console.log(`Server is running on ${port}`));
+server.listen(port, () => console.log(`Server is running on ${port}`));
